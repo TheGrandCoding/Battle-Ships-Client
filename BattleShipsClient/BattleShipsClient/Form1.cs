ff90581fd7f7dc321867082dad294a1b6c5ca495
abdulshafiq0 <43822591+abdulshafiq0@users.noreply.github.com>
2019-10-03 17:59:42 +0100

    AddedChooeShips
    Used 4th as example which can be changed depending on which ship needs to be chosen.

--- BattleShipsClient/BattleShipsClient/Form1.cs
+++ BattleShipsClient/BattleShipsClient/Form1.cs
@@ -32,6 +32,8 @@ namespace BattleShipsClient
                     Button OppBtn = new Button();
                     userBtn.Click += UserBtn_Click;
                     OppBtn.Click += OppBtn_Click;
+                    userBtn.Name =i.ToString() +","+ j.ToString();
+                    OppBtn.Name = i.ToString() + "," + j.ToString();
                     userShips.Controls.Add(userBtn, i, j);
                     oppShips.Controls.Add(OppBtn, i, j);
                 }
@@ -41,10 +43,33 @@ namespace BattleShipsClient
         {
             MessageBox.Show("opp");
         }
-
+        int FirstX , SecoundX , FirstY , SecoundY;
+        bool FirstClicked = false;
         private void UserBtn_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("user");
+            if (sender is Button btn)
+            {
+                var Name = btn.Name.Split(',');
+                if(FirstClicked == false)
+                {
+                    FirstX = Convert.ToInt32(Name[0]);
+                    FirstY =Convert.ToInt32(Name[1]);
+                    FirstClicked = true;
+                }
+                else
+                {
+                    SecoundX = Convert.ToInt32(Name[0]);
+                    SecoundY = Convert.ToInt32(Name[1]);
+                    if(FirstX == SecoundX || FirstY == SecoundY)
+                    {
+                        if(FirstX +3 == SecoundX || FirstX - 3 == SecoundX || FirstY + 3 == SecoundY || FirstY - 3 == SecoundY)
+                        {
+                            MessageBox.Show("True");
+                        }
+                    }
+                    FirstClicked = false;
+                }
+            }
         }
         public void Con()
         {
