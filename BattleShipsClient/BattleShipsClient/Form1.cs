477b0b0fcb588f8b982a0d3d33ffe4815a2ff48b
Anushan <anushan.m001@gmail.com>
2019-10-03 14:37:05 +0100

    Update Form1.cs

--- BattleShipsClient/BattleShipsClient/Form1.cs
+++ BattleShipsClient/BattleShipsClient/Form1.cs
@@ -20,18 +20,31 @@ namespace BattleShipsClient
         }
         TcpClient client = new TcpClient();
 
-        
+
         private void Form1_Load(object sender, EventArgs e)
         {
             //Con();
-            for (int i=0; i<10; i++)
+            for (int i = 0; i < 10; i++)
             {
-                for (int j=0; j<10; j++)
+                for (int j = 0; j < 10; j++)
                 {
-                    userShips.Controls.Add(new Button{ Text = "Hit", AutoSize = true }, i, j);
-                    oppShips.Controls.Add(new Button { Text = "Hit", AutoSize = true }, i, j);
+                    Button userBtn = new Button();
+                    Button OppBtn = new Button();
+                    userBtn.Click += UserBtn_Click;
+                    OppBtn.Click += OppBtn_Click;
+                    userShips.Controls.Add(userBtn, i, j);
+                    oppShips.Controls.Add(OppBtn, i, j);
                 }
             }
+        }
+        private void OppBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("opp");
+        }
+
+        private void UserBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("user");
         }
         public void Con()
         {
